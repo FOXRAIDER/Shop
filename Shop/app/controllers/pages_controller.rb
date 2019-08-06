aefa14265d9f7a22e78d301cdbe6130067fe733c
FOXRAIDER <foxraider98@yandex.ru>
2019-08-06 18:08:54 +0400

    add footer, methods: create, save, view. Update design.

---
+++ Shop/app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+    def about
+        @page = Page.all
+    end
+
+    def new
+        
+    end
+
+    def show
+        @page = Page.find(params[:id])
+        
+    end
+
+    def create
+        #render plain: params[:page].inspect
+        @page = Page.new(page_params)
+
+        @page.save
+        redirect_to @page
+    end
+
+    private def page_params
+        params.require(:page).permit(:title, :body)
+    end
+end
--- Shop/config/routes.rb
+++ Shop/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-get 'u' => 'new_shop#q'
+    get 'u' => 'new_shop#q'
+    resources :pages
+    root 'posts#main_page', as: 'home'
+    get 'about' => 'pages#about'
 end
